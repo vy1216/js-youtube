d41cd313e311586d7d01aa0fd4237ef7b3c8507c
vy1216 <vy6490134@gmail.com>
2025-03-03 14:10:51 +0000

    dataty

--- .devcontainer/01_basics/03_conversionoperation.js
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "vinay"
 
 let booleanInLoggedIn = Boolean(isLoggedIn)
-console.log(booleanInLoggedIn);
+//console.log(booleanInLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -23,5 +23,39 @@ console.log(booleanInLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//                 Operations
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " vinay"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+
+// console.log((3+4) * 5 % 3 )
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
