0d06e54c7311a5dba81557052a63891062138554
vy1216 <vy6490134@gmail.com>
2025-03-03 20:23:49 +0000

    string

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,37 @@
+const name = "vinay"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount} `)
+
+const gameName = new String ('vinay-hc-com')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('n'));
+
+ const newString = gameName.substring(0,4)
+// console.log(newString);
+
+ const anotherString = gameName.slice(-7, 4)
+console.log(anotherString);
+
+const newStringOne = "  vinay  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://vinay.com/vinay%20yadav"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('vinay'))
+
+console.log(gameName.split('-'));
+
+
+
+
+
