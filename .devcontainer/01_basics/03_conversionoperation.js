bdb3dfdf27bc1022d00d69861d7039a8c07a9068
vy1216 <vy6490134@gmail.com>
2025-03-03 08:08:55 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,27 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "3abc" => Nan
+// true => 1; false => 0
+
+let isLoggedIn = "vinay"
+
+let booleanInLoggedIn = Boolean(isLoggedIn)
+console.log(booleanInLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "vinay" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
