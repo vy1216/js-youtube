cce06d762b9c6584fd5f9cb37f6c91930d84ee67
vy1216 <vy6490134@gmail.com>
2025-03-03 06:06:59 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+ "use strict";// trat all JS code as newer version
+
+//  alert(3+3) // we are using nodejs not browser
+let name = "vinay"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string = ""
+//  boolean => true/false
+//  null =. standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
