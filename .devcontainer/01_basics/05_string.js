d88a603413adf0b6317ba4c062b9f757a32db3e3
vy1216 <vy6490134@gmail.com>
2025-03-11 12:35:51 +0000

    math and num

--- .devcontainer/01_basics/05_string.js
+++ .devcontainer/01_basics/05_string.js
@@ -6,8 +6,8 @@ const repoCount = 50
 // console.log(`Hello my name is ${name} and my repo count is ${repoCount} `)
 
 const gameName = new String ('vinay-hc-com')
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
@@ -20,16 +20,16 @@ const gameName = new String ('vinay-hc-com')
  const anotherString = gameName.slice(-7, 4)
 console.log(anotherString);
 
-const newStringOne = "  vinay  "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const newStringOne = "  vinay  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
-const url = "https://vinay.com/vinay%20yadav"
-console.log(url.replace('%20', '-'));
+// const url = "https://vinay.com/vinay%20yadav"
+// console.log(url.replace('%20', '-'));
 
-console.log(url.includes('vinay'))
+// console.log(url.includes('vinay'))
 
-console.log(gameName.split('-'));
+// console.log(gameName.split('-'));
 
 
 
